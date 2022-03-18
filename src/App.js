8d8dc28282928b1b058b4c5cbac8e57578750916
Caia Cristina <cristinaelenacaia@gmail.com>
2022-03-18 15:13:42 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import Counter from './Counter' // ./ means the current folder
+import CounterHooks from './CounterHooks'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ console.log("Render App")
+  return ( 
+    <>
+    Counter
+    <Counter initialCount = {0} />
+    Counter Hooks
+    <CounterHooks initial Count = {3} />
+    </>
+    
+  )
+  
 }
 
 export default App;
